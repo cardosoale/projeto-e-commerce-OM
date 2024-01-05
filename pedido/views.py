0fc9c96709486bbe8318b2634bf70d9d697f9e58
Alexandre Cardoso <cardoal01@gmail.com>
2024-01-05 12:07:52 -0300

    570

--- pedido/views.py
+++ pedido/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.views import View
 
-# Create your views here.
+
+class Pagar(View):
+    ...
+
+
+class FecharPedido(View):
+    ...
+
+
+class Detalhe(View):
+    ...
--- perfil/views.py
+++ perfil/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.views import View
 
-# Create your views here.
+
+class Criar(View):
+    ...
+
+
+class Atualizar(View):
+    ...
+
+
+class Login(View):
+    ...
+
+
+class Logout(View):
+    ...
