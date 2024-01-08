19b18cf9d24572a17efdbeb881f2dc355e138dbf
Alexandre Cardoso <cardoal01@gmail.com>
2024-01-08 14:25:16 -0300

    571

--- produto/views.py
+++ produto/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
 from django.views.generic import ListView
 from django.views import View
+from . import models
 
 
 class ListaProdutos(ListView):
-    ...
+    model = models.Produto
+    template_name = 'produto/lista.html'
+    context_object_name = 'produtos'
+    paginate_by = 9
 
 
 class DetalheProduto(View):
