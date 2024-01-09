771a0427417b959ffad0ad7360f8b9c78452e1b0
Alexandre Cardoso <cardoal01@gmail.com>
2024-01-09 12:33:09 -0300

    572

--- produto/views.py
+++ produto/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from django.views import View
 from . import models
 
@@ -11,8 +11,11 @@ class ListaProdutos(ListView):
     paginate_by = 9
 
 
-class DetalheProduto(View):
-    ...
+class DetalheProduto(DetailView):
+    model = models.Produto
+    template_name = 'produto/detalhe.html'
+    context_object_name = 'produto'
+    slug_url_kwarg = 'slug'
 
 
 class AdicionarAoCarrinho(View):
