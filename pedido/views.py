55d87449a4f5347c695d8e0c966cf94f3242614e
Alexandre Cardoso <cardoal01@gmail.com>
2024-01-15 10:31:23 -0300

    581

--- pedido/views.py
+++ pedido/views.py
@@ -1,14 +1,18 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 from django.views import View
 
 
 class Pagar(View):
-    ...
+    def get(self, *args, **kwargs):
+        return HttpResponse('Pagar')
 
 
 class SalvarPedido(View):
-    ...
+    def get(self, *args, **kwargs):
+        return HttpResponse('Salvar Pedido')
 
 
 class Detalhe(View):
-    ...
+    def get(self, *args, **kwargs):
+        return HttpResponse('Detalhe')
